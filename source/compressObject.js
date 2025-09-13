d91bfdf77726e9dbbcbe7c9c1e2827ff30003be1
Евгения Озерина <e.ozerina@vk.team>
2025-09-13 17:41:44 +0300

    linter fix

--- source/compressObject.js
+++ source/compressObject.js
@@ -1,42 +1,41 @@
-'use strict';
+"use strict";
 
 /**
  * Функция сжатия объекта
- * @param {Object} - объект
- * 
+ * @param {Object} obj - объект
+ *
  * @example
  * // returns {a: "test"}
  * compressObject({a: "test", b: undefined, c: null, d: ""})
- * 
+ *
  * @returns {Object} - объект, содержащий только ключи с ненулевыми значениями
  */
 const compressObject = (obj) => {
-    const result = {};
+  const result = {};
 
-    if (!isObject(obj)) {
-      throw new TypeError(`Аргумент не является объектом`);
+  if (!isObject(obj)) {
+    throw new TypeError(`Аргумент не является объектом`);
+  }
+
+  for (let key in obj) {
+    if (obj[key] !== null && obj[key] !== undefined && obj[key] !== "") {
+      result[key] = obj[key];
     }
+  }
 
-    for (let key in obj) {
-        if (obj[key] !== null && obj[key] !== undefined && obj[key] !== "") {
-            result[key] = obj[key];
-        }
-    }
-
-    return result;
-}
+  return result;
+};
 
 /**
  * Функция проверки, является ли аргумент функции объектом
- * @param {any} - значение
- * 
+ * @param {any} param - значение
+ *
  * @example
  * // returns false
  * isObject('string')
- * 
+ *
  * @returns {boolean} - true если объект, false если не-объект
  */
-
 const isObject = (param) => {
-    return typeof(param) === 'object' && param !== null;
-}
+  return typeof param === "object" && param !== null;
+};
